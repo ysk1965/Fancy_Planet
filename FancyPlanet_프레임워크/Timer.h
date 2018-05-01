85ab43c35ce24521c330cea68f40ea8e98a3bca6
irenepeace1214 <qweas50@naver.com>
2018-05-02 03:05:49 +0900

    Revert "Add files via upload"
    This reverts commit ae599ed3cd853cb5a171afa7da0481bd1282946f.

--- FancyPlanet_프레임워크/Timer.h
+++ FancyPlanet_프레임워크/Timer.h
@@ -1,49 +1,42 @@
 #pragma once
-#include<windows.h>
-#define DECLARE_SINGLETON(ClassName)			\
-private:										\
-	static ClassName*		m_pInstance;		\
-public:											\
-	static ClassName*		GetInstance(void)	\
-	{											\
-		if(m_pInstance == NULL)					\
-			m_pInstance = new ClassName;		\
-		return m_pInstance;						\
-	}											\
-	void	DestroyInstance(void)				\
-	{											\
-		if(m_pInstance != NULL)					\
-		{										\
-			delete m_pInstance;					\
-			m_pInstance = NULL;					\
-		}										\
-	}											\
+//-----------------------------------------------------------------------------
+// File: CGameTimer.h
+//-----------------------------------------------------------------------------
 
+const ULONG MAX_SAMPLE_COUNT = 50; // Maximum frame time sample count
 
-#define  IMPLEMENT_SINGLETON(ClassName)			\
-ClassName*	ClassName::m_pInstance = NULL;		\
-
-#define GETTIME			CTimeMgr::GetInstance()->GetTime()
-
-class CTimeMgr
+class CGameTimer
 {
-	DECLARE_SINGLETON(CTimeMgr)
-private:
-	CTimeMgr();
-	~CTimeMgr();
 public:
-	float GetTime(void);
+	CGameTimer();
+	virtual ~CGameTimer();
 
-public:
-	void InitTime(void);
-	void SetTime(void);
+	void Tick(float fLockFPS = 0.0f);
+	void Start();
+	void Stop();
+	void Reset();
 
+	unsigned long GetFrameRate(LPTSTR lpszString = NULL, int nCharacters = 0);
+	float GetTimeElapsed();
+	float GetTotalTime();
 private:
-	LARGE_INTEGER		m_FrameTime;
-	LARGE_INTEGER		m_FixTime;
-	LARGE_INTEGER		m_LastTime;
-	LARGE_INTEGER		m_CpuTick;
+	double							m_fTimeScale;
+	float							m_fTimeElapsed;
 
-	float		m_fTime;
+	__int64							m_nBasePerformanceCounter;
+	__int64							m_nPausedPerformanceCounter;
+	__int64							m_nStopPerformanceCounter;
+	__int64							m_nCurrentPerformanceCounter;
+	__int64							m_nLastPerformanceCounter;
+
+	__int64							m_nPerformanceFrequencyPerSec;
+
+	float							m_fFrameTime[MAX_SAMPLE_COUNT];
+	ULONG							m_nSampleCount;
+
+	unsigned long					m_nCurrentFrameRate;
+	unsigned long					m_nFramesPerSecond;
+	float							m_fFPSTimeElapsed;
+
+	bool							m_bStopped;
 };
-
