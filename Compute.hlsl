da7bc953aaa0d717d7ada433eeabaa5fd7d8268b
Bak-sinseok <newstone1993@naver.com>
2018-07-07 23:53:44 +0900

    Add files via upload

---
+++ Compute.hlsl
@@ -0,0 +1,25 @@
+
+Texture2D gInput            : register(t0);
+RWTexture2D<float4> gOutput : register(u0);
+cbuffer cbSettings : register(b0)
+{
+	float value;
+};
+#define N 256
+
+[numthreads(N, 1, 1)]
+void CS(int3 groupThreadID : SV_GroupThreadID, int3 dispatchThreadID : SV_DispatchThreadID)
+{
+	float4 fResult;
+
+	//fResult = gInput[int2(dispatchThreadID.x, dispatchThreadID.y)] + gInput[int2(dispatchThreadID.x - (int)value, dispatchThreadID.y - (int)value)] +
+	//	gInput[int2(dispatchThreadID.x, dispatchThreadID.y - (int)value)] + gInput[int2(dispatchThreadID.x + (int)value, dispatchThreadID.y - (int)value)] +
+	//		gInput[int2(dispatchThreadID.x + (int)value, dispatchThreadID.y)] + gInput[int2(dispatchThreadID.x + (int)value, dispatchThreadID.y + (int)value)]+
+	//			gInput[int2(dispatchThreadID.x, dispatchThreadID.y + (int)value)] + gInput[int2(dispatchThreadID.x - (int)value, dispatchThreadID.y + (int)value)]+
+	//				gInput[int2(dispatchThreadID.x - (int)value, dispatchThreadID.y)];
+	//
+	//fResult /= 9.0f;
+
+	fResult = max(0, gInput[dispatchThreadID.xy] - 0.004f);
+	gOutput[dispatchThreadID.xy] = fResult;
+}
